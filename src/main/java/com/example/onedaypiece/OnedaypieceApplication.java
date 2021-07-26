fb0d522a4f473a1fbb1d68f0d0477a33cdcbde60
icwgi <dhkdrb897@naver.com>
2021-07-26 23:35:21 +0900

    start

---
+++ src/main/java/com/example/onedaypiece/OnedaypieceApplication.java
@@ -0,0 +1,13 @@
+package com.example.onedaypiece;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnedaypieceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnedaypieceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/onedaypiece/OnedaypieceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onedaypiece;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnedaypieceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
