ce24741de96af1dfa170193c29b116ddf3f7407c
Nhahan <kisy324@naver.com>
2021-07-28 09:58:23 +0900

    스케줄러 추가 2

--- src/main/java/com/example/onedaypiece/OnedaypieceApplication.java
+++ src/main/java/com/example/onedaypiece/OnedaypieceApplication.java
@@ -2,8 +2,11 @@ package com.example.onedaypiece;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.ConcurrentTaskScheduler;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableSwagger2
@@ -16,4 +19,8 @@ public class OnedaypieceApplication {
         SpringApplication.run(OnedaypieceApplication.class, args);
     }
 
+    @Bean
+    public TaskScheduler taskScheduler() {
+        return new ConcurrentTaskScheduler();
+    }
 }
