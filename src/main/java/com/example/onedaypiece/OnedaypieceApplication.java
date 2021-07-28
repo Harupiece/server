ac0d7db7e440f638c36409e27ecfc23d1f1fa313
Nhahan <kisy324@naver.com>
2021-07-28 09:53:21 +0900

    스케줄러 추가

--- src/main/java/com/example/onedaypiece/OnedaypieceApplication.java
+++ src/main/java/com/example/onedaypiece/OnedaypieceApplication.java
@@ -3,9 +3,11 @@ package com.example.onedaypiece;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableSwagger2
+@EnableScheduling
 @EnableJpaAuditing
 @SpringBootApplication
 public class OnedaypieceApplication {
---
+++ src/main/java/com/example/onedaypiece/util/Scheduler.java
@@ -0,0 +1,12 @@
+package com.example.onedaypiece.util;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class Scheduler {
+//    @Scheduled(cron = "1 0 0 * * *") // 초, 분, 시, 일, 월, 주 순서
+//    public void tempScheduler() {
+//    }
+}
