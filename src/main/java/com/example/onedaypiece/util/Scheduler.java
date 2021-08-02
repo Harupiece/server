d023a9f6d62dae84a9c686306cd81705eea86589
Nhahan <kisy324@naver.com>
2021-08-02 16:24:21 +0900

    @EnableAsync

--- src/main/java/com/example/onedaypiece/util/Scheduler.java
+++ src/main/java/com/example/onedaypiece/util/Scheduler.java
@@ -8,7 +8,6 @@ import com.example.onedaypiece.web.domain.posting.Posting;
 import com.example.onedaypiece.web.domain.posting.PostingRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.tomcat.jni.Local;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
