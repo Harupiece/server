07212b18af25480d56fd084e518e7cc3080508e5
icwgi <dhkdrb897@naver.com>
2021-08-02 19:34:14 +0900

    리펙토링 + security보안 모든유저제거

--- src/main/java/com/example/onedaypiece/web/dto/request/mypage/ProfileUpdateRequestDto.java
+++ src/main/java/com/example/onedaypiece/web/dto/request/mypage/ProfileUpdateRequestDto.java
@@ -7,6 +7,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 
@@ -14,21 +15,12 @@ import javax.validation.constraints.NotNull;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class ProfileUpdateRequestDto {
 
-    @NotNull(message = "닉네임은 null이거나 빈값은 불가능합니다.")
+    @NotBlank(message = "닉네임은 null이거나 빈값은 불가능합니다.")
     private String nickname;
-    @NotNull(message = "프로필이미지는 null이거나 빈값은 불가능합니다. ")
+    @NotBlank(message = "프로필이미지는 null이거나 빈값은 불가능합니다. ")
     private String profileImage;
 
     public ProfileUpdateRequestDto(String nickname, String profileImage){
-
-        if(nickname.isEmpty()){
-            throw new ApiRequestException("변경할 닉네임을 입력해주세요.");
-        }
-        
-        if(profileImage.isEmpty()){
-            throw new ApiRequestException("프로필 이미지를 넣어주세요.");
-        }
-
         this.nickname = nickname;
         this.profileImage = profileImage;
     }
