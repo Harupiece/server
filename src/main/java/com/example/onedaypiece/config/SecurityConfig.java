00ebc84eb9725cab6c4ad44df4007c9d44ce96b9
swcide <swcide@gmail.com>
2021-08-03 11:06:54 +0900

    Revert "Revert "메인 권한 all""
    This reverts commit 1640deee0e9be2d5c2d52e3420aeb242981c08f8.

--- src/main/java/com/example/onedaypiece/config/SecurityConfig.java
+++ src/main/java/com/example/onedaypiece/config/SecurityConfig.java
@@ -63,6 +63,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/api/member/signup/**").permitAll()
                 .antMatchers("/api/member/login/**").permitAll()
                 .antMatchers("/api/member/reissue").permitAll()
+                .antMatchers("/api/guest/main").permitAll()
                 //여기에카카오 요청넣어보기
                 .anyRequest().authenticated()   // 나머지 API 는 전부 인증 필요
 
