a637407c5eb3f5a23e92b6f23bbe040e864950b6
Nhahan <kisy324@naver.com>
2021-08-17 01:49:55 +0900

    젠킨스 잘되는 버전

--- src/main/java/com/example/onedaypiece/web/domain/pointHistory/PointHistoryRepository.java
+++ src/main/java/com/example/onedaypiece/web/domain/pointHistory/PointHistoryRepository.java
@@ -1,27 +1,12 @@
 package com.example.onedaypiece.web.domain.pointHistory;
 
 import com.example.onedaypiece.web.dto.response.mypage.histroy.MemberHistoryDto;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface PointHistoryRepository extends JpaRepository<PointHistory, Long> {
-
-    //3 차
-    @Query("select p, " +
-            "p.certification.member, " +
-            "p.certification.member.point, " +
-            "p.certification.posting.challenge, " +
-            "p.certification.posting " +
-            "from PointHistory p " +
-            "join fetch p.certification " +
-            "where p.certification.member.email = :email")
-    List<PointHistory> find(String email);
-
-
 
     @Query("select new com.example.onedaypiece.web.dto.response.mypage.histroy.MemberHistoryDto( " +
             "p.pointHistoryId," +
@@ -35,22 +20,5 @@ public interface PointHistoryRepository extends JpaRepository<PointHistory, Long
             "p.posting.member.point) " +
             "from PointHistory p " +
             "where p.posting.member.email =:email")
-
     List<MemberHistoryDto> findHistory(String email);
-
-
-    @Query("select new com.example.onedaypiece.web.dto.response.mypage.histroy.MemberHistoryDto( " +
-            "p.pointHistoryId," +
-            "p.createdAt, " +
-            "p.certification.posting.challenge.challengeTitle, " +
-            "p.getPoint," +
-            "p.certification.member.memberId," +
-            "p.certification.member.nickname," +
-            "p.certification.member.profileImg, " +
-            "p.certification.member.point.acquiredPoint," +
-            "p.certification.member.point) " +
-            "from PointHistory p " +
-            "where p.certification.member.email =:email")
-
-    List<MemberHistoryDto> findHistory2(String email, Pageable pageable);
 }
