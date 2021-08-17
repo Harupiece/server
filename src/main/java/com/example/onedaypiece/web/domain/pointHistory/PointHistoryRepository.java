3bda6ffa9bcde15b438e3a7e4928486d5c5d8b2a
swcide <swcide@gmail.com>
2021-08-17 12:40:08 +0900

    멤버 마이페이지 히스토리 . + 스케줄러 쿼리

--- src/main/java/com/example/onedaypiece/web/domain/pointHistory/PointHistoryRepository.java
+++ src/main/java/com/example/onedaypiece/web/domain/pointHistory/PointHistoryRepository.java
@@ -20,17 +20,19 @@ public interface PointHistoryRepository extends JpaRepository<PointHistory, Long
             "p.posting.member.point.acquiredPoint," +
             "p.posting.member.point) " +
             "from PointHistory p " +
-            "join p.challengeRecord " +
-            "where p.posting.member.email =:email")
+            "left join p.challengeRecord cr " +
+            "left join p.posting p2 " +
+            "left join p.challengeRecord.member " +
+            "left join p.posting.member " +
+            "left join p.posting.member.point " +
+            "left join p.posting.challenge " +
+            "where p.posting.member.email =:email " +
+            "or p.challengeRecord.member.email = :email" )
     List<MemberHistoryDto> findHistory(String email);
 
 
     @Query("select ph " +
-            "from PointHistory ph left join ph.challengeRecord cr " +
-            "left join ph.posting p " +
-            "left join Member m on m.memberId = p.member.memberId " +
-            "left join Member m2 on m2.memberId = cr.member.memberId " +
-            "where  p.member =:member or cr.member =:member")
+            "from PointHistory ph ")
     List<PointHistory> findHistory2(Member member);
 
 
