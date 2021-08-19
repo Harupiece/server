66434bf182b2327a7d1cdddc687dd6953a8191de
Nhahan <kisy324@naver.com>
2021-08-19 21:15:49 +0900

    scheduler에서 공식챌린지 초기세팅

--- src/main/java/com/example/onedaypiece/web/domain/challengeRecord/ChallengeRecordQueryRepository.java
+++ src/main/java/com/example/onedaypiece/web/domain/challengeRecord/ChallengeRecordQueryRepository.java
@@ -179,7 +179,7 @@ public class ChallengeRecordQueryRepository {
                 .selectFrom(challengeRecord)
                 .where(challengeRecord.challengeRecordStatus.eq(true),
                         challengeRecord.member.eq(member),
-                        challengeRecord.challenge.challengeProgress.eq(progress))
+                        challengeRecord.challenge.challengeProgress.lt(progress))
                 .fetch();
     }
 
